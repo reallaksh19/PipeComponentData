80cec9292fc6c1082592cf1f72c389b859f21428
reallaksh19 <reallaksh19@gmail.com>
2026-06-11 20:09:25 +0400

    Expose support defaults dataset

--- src/db/datasets/index.js
+++ src/db/datasets/index.js
@@ -3,6 +3,7 @@ import { VALVES } from './valves.js';
 import { FLANGES } from './flanges.js';
 import { FITTINGS } from './fittings.js';
 import { COMPONENT_WEIGHTS } from './weights.js';
+import { SUPPORT_DEFAULTS } from './supportDefaults.js';
 
 export const PHASE4_DATASETS = Object.freeze({
   pipeSchedules: PIPE_SCHEDULES,
@@ -10,4 +11,5 @@ export const PHASE4_DATASETS = Object.freeze({
   flanges: FLANGES,
   fittings: FITTINGS,
   componentWeights: COMPONENT_WEIGHTS,
+  supportDefaults: SUPPORT_DEFAULTS,
 });
