3788e1c276099b2c4d4a3f895a79d975cf828853
reallaksh19 <reallaksh19@gmail.com>
2026-06-11 20:01:39 +0400

    Phase 4: add dataset index

---
+++ src/db/datasets/index.js
@@ -0,0 +1,13 @@
+import { PIPE_SCHEDULES } from './pipeSchedules.js';
+import { VALVES } from './valves.js';
+import { FLANGES } from './flanges.js';
+import { FITTINGS } from './fittings.js';
+import { COMPONENT_WEIGHTS } from './weights.js';
+
+export const PHASE4_DATASETS = Object.freeze({
+  pipeSchedules: PIPE_SCHEDULES,
+  valves: VALVES,
+  flanges: FLANGES,
+  fittings: FITTINGS,
+  componentWeights: COMPONENT_WEIGHTS,
+});
