74ddc7d5dd94c1281807b2925779bae5404d1164
reallaksh19 <reallaksh19@gmail.com>
2026-06-18 20:45:11 +0400

    fix: keep gateValveSvg as sync re-export

--- pipetools/js/svg.js
+++ pipetools/js/svg.js
@@ -2,8 +2,4 @@ export { iconSvg, listIconKeys } from './svg/icons.js';
 export { pipeSpanSvg } from './svg/pipeSpan.js';
 export { getSvgRenderer, hasSvgRenderer, listSvgKeys, resolveSvgKey } from './svg/registry.js';
 export { renderSvgPreview } from './svg/inspector.js';
-
-export async function gateValveSvg(row = {}) {
-  const mod = await import('./svg/inspector.js');
-  return mod.renderSvgPreview(row);
-}
+export { renderSvgPreview as gateValveSvg } from './svg/inspector.js';
